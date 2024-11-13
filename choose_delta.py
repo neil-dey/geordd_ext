8d352d7a48496fede0d88d163f3b321fcd4d2e5d
neil-dey <neild799@gmail.com>
2024-11-13 17:13:51 -0500

    A working simulation

--- choose_delta.py
+++ choose_delta.py
@@ -4,20 +4,26 @@ import matplotlib.pyplot as plt
 
 np.random.seed(0)
 
-delta = 5
+delta = 10
 epsilon = 0.1
 
 lambda_0 = 5
 tau = 10
 
+def confounding(x):
+    return 0
+    return np.exp(x)
+
 deltas = []
 tau_estimates = []
+bad_tau_estimates = []
+
 for mc_iter in range(10):
     print(mc_iter)
     # Generate some sample data
     pos_points = [st.expon.rvs(scale = 1/(lambda_0 + tau))]
     while pos_points[-1] < delta:
-        pos_points.append(pos_points[-1] + st.expon.rvs(scale = 1/(lambda_0+tau)))
+        pos_points.append(pos_points[-1] + st.expon.rvs(scale = 1/(lambda_0+tau + confounding(pos_points[-1]))))
 
     neg_points = [-1*st.expon.rvs(scale = 1/lambda_0)]
     while neg_points[-1] > -1*delta:
@@ -25,46 +31,51 @@ for mc_iter in range(10):
 
     epsilons = np.array([(n+1)*epsilon for n in range(int(delta/epsilon))])
     thetahats = np.array([len([x for x in pos_points if x < (n+1)*epsilon]) - len([x for x in neg_points if x > -(n+1)*epsilon]) for n in range(int(delta/epsilon))])
+    tauhats = [thetahat/epsilon for (thetahat, epsilon) in zip(thetahats, epsilons)]
 
     pvals = []
     for k in range(2, int(delta/epsilon)):
         # Estimate the distribution of beta hat
-        covs = np.array([(i+1)*epsilon for i in range(k)])
-        Xt = np.stack((np.ones(k)*1.0, covs))
-        hat = np.linalg.inv(Xt @ Xt.T) @ Xt
         theory = []
-
         pos_mean = len([x for x in pos_points if x < (k+1)*epsilon])/((k+1)*epsilon)
         neg_mean = len([x for x in neg_points if x > -(k+1)*epsilon])/((k+1)*epsilon)
-
         pval_iters = 100
         for _ in range(pval_iters):
-            theory.append(sum([(12*i-6*k+6)/((k-1)*k*(k+1)*epsilon) * (st.poisson.rvs((pos_mean)*(i+1)*epsilon) - st.poisson.rvs(neg_mean*(i+1)*epsilon)) for i in range(k)]))
+            #theory.append(sum([(12*i-6*k+6)/((k-1)*k*(k+1)*epsilon) * (st.poisson.rvs((pos_mean)*(i+1)*epsilon) - st.poisson.rvs(neg_mean*(i+1)*epsilon)) for i in range(k)]))
+            theory.append(sum([(12*i-6*k+6)/((k-1)*k*(k+1)*epsilon * (i+1)*epsilon) * (st.poisson.rvs((lambda_0+tau)*(i+1)*epsilon) - st.poisson.rvs(lambda_0*(i+1)*epsilon)) for i in range(k)]))
 
-        # Compute the actual beta hat
-        beta1 = np.linalg.lstsq(np.vstack([np.ones(k), epsilons[:k]]).T, thetahats[:k], rcond = None)[0][1]
+        # Compute p-value for H_0: beta = 0
+        beta1 = np.linalg.lstsq(np.vstack([np.ones(k), epsilons[:k]]).T, tauhats[:k], rcond = None)[0][1]
+        theory = sorted(theory)
+        quantile = np.searchsorted(theory, beta1)/len(theory)
+        if quantile > 0.5:
+            quantile = 1 - quantile
+        pval = 2*quantile
 
-        if beta1 > 0:
-            pval = len([beta for beta in theory if beta > beta1 or beta < -1*beta1])/pval_iters
-        else:
-            pval = len([beta for beta in theory if beta < beta1 or beta > -1*beta1])/pval_iters
         pvals.append(pval)
 
-    pvals = st.false_discovery_control(pvals)
+    # We should investigate what p-value combiner is most appropriate
+    pvals = st.false_discovery_control(pvals, method = 'by')
     k = 2
     for idx, pval in enumerate(pvals):
         if pval < 0.05:
             break
         k += 1
 
-    #print("    ", epsilons[k-1])
-    #print("    ", thetahats[k-1]/epsilons[k-1])
+    print("    ", pvals[::10])
+    print("    ", epsilons[k-1])
+    print("    ", thetahats[k-1]/epsilons[k-1])
+    print("    ", thetahats[-1]/epsilons[-1])
     deltas.append(epsilons[k-1])
     tau_estimates.append(thetahats[k-1]/epsilons[k-1])
+    bad_tau_estimates.append(thetahats[-1]/epsilons[-1])
 
-print(deltas)
-print(np.mean(deltas))
 print()
-print(tau_estimates)
-print(np.mean(tau_estimates))
-print(np.mean([(t - tau)**2 for t in tau_estimates]))
+print("Final results")
+print("Avg. Delta:", np.mean(deltas))
+print()
+print("Proposal Mean tau:", np.mean(tau_estimates))
+print("Proposal tau MSE:", np.mean([(t - tau)**2 for t in tau_estimates]))
+print()
+print("Naive Mean tau", np.mean(bad_tau_estimates))
+print("Naive tau MSE", np.mean([(t - tau)**2 for t in bad_tau_estimates]))
--- poisson.py
+++ poisson.py
@@ -2,9 +2,9 @@ import numpy as np
 import scipy.stats as st
 import matplotlib.pyplot as plt
 
-np.random.seed(0)
+#np.random.seed(0)
 
-delta = 50
+delta = 10
 epsilon = 0.1
 
 lambda_0 = 5
@@ -12,12 +12,16 @@ tau = 10
 
 k = 5
 
+def confounding(x):
+    return 0
+    return np.exp(x)
+
 beta1s = []
 thetas = []
 for mc_iter in range(1000):
     pos_points = [st.expon.rvs(scale = 1/(lambda_0 + tau))]
     while pos_points[-1] < delta:
-        pos_points.append(pos_points[-1] + st.expon.rvs(scale = 1/(lambda_0+tau)))
+        pos_points.append(pos_points[-1] + st.expon.rvs(scale = 1/(lambda_0+tau + confounding(pos_points[-1]))))
 
     neg_points = [-1*st.expon.rvs(scale = 1/lambda_0)]
     while neg_points[-1] > -1*delta:
@@ -25,13 +29,16 @@ for mc_iter in range(1000):
 
     epsilons = np.array([(n+1)*epsilon for n in range(int(delta/epsilon))])
     thetahats = np.array([len([x for x in pos_points if x < (n+1)*epsilon]) - len([x for x in neg_points if x > -(n+1)*epsilon]) for n in range(int(delta/epsilon))])
+    tauhats = [theta/epsilon for (theta, epsilon) in zip(thetahats, epsilons)]
 
-    beta1 = np.linalg.lstsq(np.vstack([np.ones(k), epsilons[:k]]).T, thetahats[:k], rcond = None)[0]
+    #beta1 = np.linalg.lstsq(np.vstack([np.ones(k), epsilons[:k]]).T, thetahats[:k], rcond = None)[0]
+    beta1 = np.linalg.lstsq(np.vstack([np.ones(k), epsilons[:k]]).T, tauhats[:k], rcond = None)[0]
     beta1s.append(beta1[1])
 
+    """
     if mc_iter == 1:
         k = int(delta/epsilon)
-        thetahats = [theta/epsilon for (theta, epsilon) in zip(thetahats, epsilons)]
+        #thetahats = [theta/epsilon for (theta, epsilon) in zip(thetahats, epsilons)]
         beta1 = np.linalg.lstsq(np.vstack([np.ones(k), epsilons[:k]]).T, thetahats[:k], rcond = None)[0]
         epsilons = epsilons
         thetahats = thetahats
@@ -40,6 +47,7 @@ for mc_iter in range(1000):
         plt.savefig("thetahat_plot.png")
         plt.show()
         exit()
+    """
 
 
 plt.hist(beta1s)
@@ -48,10 +56,10 @@ plt.hist(beta1s)
 covs = np.array([(i+1)*epsilon for i in range(k)])
 Xt = np.stack((np.ones(k)*1.0, covs))
 hat = np.linalg.inv(Xt @ Xt.T) @ Xt
-print(hat)
 theory = []
 for _ in range(1000):
-    b = sum([(12*i-6*k+6)/((k-1)*k*(k+1)*epsilon) * (st.poisson.rvs((lambda_0+tau)*(i+1)*epsilon) - st.poisson.rvs(lambda_0*(i+1)*epsilon)) for i in range(k)])
+    #b = sum([(12*i-6*k+6)/((k-1)*k*(k+1)*epsilon) * (st.poisson.rvs((lambda_0+tau)*(i+1)*epsilon) - st.poisson.rvs(lambda_0*(i+1)*epsilon)) for i in range(k)])
+    b = sum([(12*i-6*k+6)/((k-1)*k*(k+1)*epsilon * (i+1)*epsilon) * (st.poisson.rvs((lambda_0+tau)*(i+1)*epsilon) - st.poisson.rvs(lambda_0*(i+1)*epsilon)) for i in range(k)])
     theory.append(b)
 
 plt.hist(theory, alpha = 0.5)
